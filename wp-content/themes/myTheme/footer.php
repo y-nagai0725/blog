c593a490feebeb9950a56c54e14b884290c6988b
y-nagai0725 <y.nagai0725@gmail.com>
2024-11-28 23:57:55 +0900

    最低限のテンプレート設定

---
+++ wp-content/themes/myTheme/footer.php
@@ -0,0 +1,8 @@
+<footer id="footer" class="footer">
+  <div class="footer__inner">
+  </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ wp-content/themes/myTheme/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <header id="header" class="header">
+    <div class="header__inner">
+    </div>
+  </header>
