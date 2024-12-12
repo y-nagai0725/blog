1dc6621cdcaec81c8d3b6d91884c47e48f620fca
y-nagai0725 <y.nagai0725@gmail.com>
2024-12-12 23:52:06 +0900

    テンプレファイル追加

---
+++ wp-content/themes/myTheme/page.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+<main class="main">
+  <div class="wrapper">
+    <div class="container">
+      <div class="contents">
+
+      </div>
+    </div>
+  </div>
+</main>
+<?php get_footer(); ?>
