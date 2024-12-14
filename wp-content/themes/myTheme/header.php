edebd2df6ec6f4458c5553e32f5b165a5a7e4859
y-nagai0725 <y.nagai0725@gmail.com>
2024-12-14 16:25:45 +0900

    header作成中

--- wp-content/themes/myTheme/header.php
+++ wp-content/themes/myTheme/header.php
@@ -10,5 +10,17 @@
 <body <?php body_class(); ?>>
   <header id="header" class="header">
     <div class="header__inner">
+      <?php
+      if (is_home() || is_front_page()) {
+        $title_tag = "h1";
+      } else {
+        $title_tag = "p";
+      }
+      ?>
+      <<?php echo $title_tag ?> class="header__title">
+        <a href="<?php echo home_url() ?>">
+          <img src="<?php echo get_template_directory_uri() ?>/images/logo_black.svg" alt="<?php echo bloginfo("name") ?>">
+        </a>
+      </<?php echo $title_tag ?>>
     </div>
   </header>
