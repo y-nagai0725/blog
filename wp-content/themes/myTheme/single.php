ffaae07cc99d1cd1df750cade7daf7e92ff65b96
y-nagai0725 <y.nagai0725@gmail.com>
2024-12-19 23:58:09 +0900

    記事ページテンプレ作成中など

--- wp-content/themes/myTheme/single.php
+++ wp-content/themes/myTheme/single.php
@@ -3,7 +3,31 @@
   <div class="wrapper">
     <div class="container">
       <div class="contents">
-
+        <?php if (have_posts()): the_post(); ?>
+          <article <?php post_class('article'); ?>>
+            <div class="article__header">
+              <div class="article__thumbnail-wrapper">
+                <?php if (has_category()): ?>
+                  <span class="article__category-tag"><?php echo get_the_category()[0]->name ?></span>
+                <?php endif; ?>
+                <?php
+                if (has_post_thumbnail()) {
+                  the_post_thumbnail('large');
+                }
+                ?>
+              </div>
+              <h1 class="article__title"><?php the_title(); ?></h1>
+              <div class="article__date-wrapper">
+                <span class="article__post-date"><?php echo get_the_date("Y.m.d") ?></span>
+                <?php if (get_the_date("Y.m.d") !== get_the_modified_time("Y.m.d")): ?>
+                  <span class="article__modified-date"><?php echo get_the_modified_time("Y.m.d") ?></span>
+                <?php endif; ?>
+              </div>
+            </div>
+            <div class="article__main"></div>
+            <div class="article__footer"></div>
+          </article>
+        <?php endif; ?>
       </div>
       <?php get_sidebar(); ?>
     </div>
