72adc1e9d1b60910792a70772151f49f05d38fb0
y-nagai0725 <y.nagai0725@gmail.com>
2024-12-28 04:05:36 +0900

    sidebar作成途中など

--- wp-content/themes/myTheme/sidebar.php
+++ wp-content/themes/myTheme/sidebar.php
@@ -6,17 +6,25 @@ $posts = get_posts(array("numberposts" => 5,));
   <div class="sidebar__inner">
     <h5 class="sidebar__heading">Author</h5>
     <div class="sidebar__author-area">
-      <div class="sidebar__author-left-box">
+      <div class="sidebar__author-bg">
         <div class="sidebar__author-image-wrapper">
-          <img src="" alt="" class="sidebar__author-image">
-          <span class="sidebar__author-name">みかん箱</span>
+          <img src="<?php echo get_template_directory_uri() ?>/images/sidebar_profile.png" alt="みかん箱 プロフィール画像" class="sidebar__author-image">
         </div>
       </div>
-      <div class="sidebar__author-right-box">
+      <div class="sidebar__author-information">
+        <span class="sidebar__author-name">みかん箱</span>
         <span class="sidebar__author-text">サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル</span>
-        <div class="sidebar__sns-link-wrapper">
-
-        </div>
+        <ul class="sidebar__sns-wrapper">
+          <li class="sidebar__sns-list">
+            <a href="" class="sidebar__sns-link"></a>
+          </li>
+          <li class="sidebar__sns-list">
+            <a href="" class="sidebar__sns-link"></a>
+          </li>
+          <li class="sidebar__sns-list">
+            <a href="" class="sidebar__sns-link"></a>
+          </li>
+        </ul>
       </div>
     </div>
     <h5 class="sidebar__heading">カテゴリー</h5>
@@ -26,10 +34,10 @@ $posts = get_posts(array("numberposts" => 5,));
       <?php endforeach; ?>
     </ul>
     <h5 class="sidebar__heading">最新の投稿</h5>
-    <ul class="sidebar__latest-post-list">
+    <ul class="sidebar__post-list">
       <?php foreach ($posts as $post): ?>
         <li class="sidebar__post">
-          <a class="sidebar__post-link" href="">
+          <a class="sidebar__post-link" href="<?php echo get_permalink($post->ID) ?>">
             <div class="sidebar__post-image-wrapper">
               <?php echo get_the_post_thumbnail($post->ID, 'medium') ?>
             </div>
