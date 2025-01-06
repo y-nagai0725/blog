d58216280b1e71d835cc56e00894cceed38ee1a4
y-nagai0725 <y.nagai0725@gmail.com>
2025-01-06 23:59:42 +0900

    クラス名、タイトル部分修正など

--- wp-content/themes/myTheme/page.php
+++ wp-content/themes/myTheme/page.php
@@ -3,14 +3,17 @@
   <div class="wrapper">
     <div class="title-area">
       <div class="title-wrapper">
-        <h1 class="title"><?php the_title(); ?></h1>
+        <h1 class="title">
+          <span class="title-en"><?php echo ucwords(get_post(get_the_ID())->post_name); ?></span>
+          <span class="title-jp"><?php the_title(); ?></span>
+        </h1>
       </div>
     </div>
     <?php echo breadcrumb(); ?>
     <div class="container">
       <div class="contents">
         <?php if (have_posts()): the_post(); ?>
-          <div class="page">
+          <div class="page-wrapper">
             <?php the_content(); ?>
           </div>
         <?php endif; ?>
