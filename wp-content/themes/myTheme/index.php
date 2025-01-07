bb221e49563a6c9214b9a4d2a3bc8ad0d24638f8
y-nagai0725 <y.nagai0725@gmail.com>
2025-01-07 19:46:46 +0900

    一覧テンプレ作成途中

--- wp-content/themes/myTheme/index.php
+++ wp-content/themes/myTheme/index.php
@@ -3,7 +3,16 @@
   <div class="wrapper">
     <div class="container">
       <div class="contents">
-
+        <ul class="loop-contents">
+          <?php
+          if (have_posts()) {
+            while (have_posts()) {
+              the_post();
+              get_template_part('loop-content');
+            }
+          }
+          ?>
+        </ul>
       </div>
       <?php get_sidebar(); ?>
     </div>
---
+++ wp-content/themes/myTheme/loop-content.php
@@ -0,0 +1,25 @@
+<li class="loop-item">
+  <a href="<?php the_permalink() ?>" class="article-link">
+    <div class="article-thumbnail-wrapper">
+      <?php if (has_category()): ?>
+        <span class="article-category"><?php echo get_the_category()[0]->name ?></span>
+      <?php endif; ?>
+      <?php
+      if (has_post_thumbnail()) {
+        the_post_thumbnail('large');
+      }else{
+        echo "<img src='". get_template_directory_uri() ."/images/no-thumbnail.jpg'>";
+      }
+      ?>
+    </div>
+    <div class="article-text-wrapper">
+      <h2 class="article-title"><?php the_title(); ?></h2>
+      <div class="article-date-wrapper">
+        <span class="post-date"><?php echo get_the_date("Y.m.d") ?></span>
+        <?php if (get_the_date("Y.m.d") !== get_the_modified_time("Y.m.d")): ?>
+          <span class="modified-date"><?php echo get_the_modified_time("Y.m.d") ?></span>
+        <?php endif; ?>
+      </div>
+    </div>
+  </a>
+</li>
