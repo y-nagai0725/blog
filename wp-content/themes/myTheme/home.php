a6c58ec2c3723ff01d1fc9bc1b74d3fe98d8adbe
y-nagai0725 <y.nagai0725@gmail.com>
2025-01-09 00:19:21 +0900

    home(トップページ)作成

--- wp-content/themes/myTheme/home.php
+++ wp-content/themes/myTheme/home.php
@@ -1,9 +1,31 @@
 <?php get_header(); ?>
 <main class="main">
   <div class="wrapper">
+    <?php echo breadcrumb(); ?>
     <div class="container">
       <div class="contents">
-
+        <ul class="loop-contents">
+          <?php
+          if (have_posts()) {
+            while (have_posts()) {
+              the_post();
+              get_template_part('loop-content');
+            }
+          }
+          ?>
+        </ul>
+        <div class="pagination">
+          <?php
+          echo paginate_links(
+            array(
+              'type' => 'list',
+              'mid_size' => '1',
+              'prev_text' => '',
+              'next_text' => ''
+            )
+          );
+          ?>
+        </div>
       </div>
       <?php get_sidebar(); ?>
     </div>
