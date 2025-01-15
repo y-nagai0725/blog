7ce8a74d60dcfb50c744e8ceb20e3bf0852ff047
y-nagai0725 <y.nagai0725@gmail.com>
2025-01-15 19:03:20 +0900

    検索結果、404ページにおススメ記事やタグ情報を追加

--- wp-content/themes/myTheme/404.php
+++ wp-content/themes/myTheme/404.php
@@ -4,12 +4,46 @@
     <?php echo breadcrumb(); ?>
     <div class="container">
       <div class="contents">
-        <div class="page-404-contents">
-          <h1 class="title">
-            <span class="title-404">404</span>
-            <span class="title-text">Not Found</span>
+        <div class="page-404">
+          <h1 class="page-404__title">
+            <span class="page-404__title-404">404</span>
+            <span class="page-404__title-text">Not Found</span>
           </h1>
-          <p class="text">該当するページが見つかりませんでした。</p>
+          <span class="page-404__text">該当するページが見つかりませんでした。</span>
+          <span class="page-404__sub-title">こちらの記事はいかがですか？</span>
+          <?php
+          $posts = get_posts(array("numberposts" => 4, "orderby" => "rand",));
+          ?>
+          <ul class="page-404__recommend-list-wrapper">
+            <?php foreach ($posts as $post): ?>
+              <li class="page-404__recommend-list">
+                <a href="<?php echo get_permalink($post->ID) ?>" class="page-404__recommend-link">
+                  <div class="page-404__recommend-image-wrapper">
+                    <?php
+                    $postThumbnail = get_the_post_thumbnail($post->ID, 'medium');
+                    if ($postThumbnail) {
+                      echo $postThumbnail;
+                    } else {
+                      echo "<img src='" . get_template_directory_uri() . "/images/no-thumbnail.jpg' alt='no-thumbnail'>";
+                    }
+                    ?>
+                  </div>
+                  <span class="page-404__recommend-title"><?php echo get_the_title($post->ID) ?></span>
+                </a>
+              </li>
+            <?php endforeach; ?>
+          </ul>
+          <span class="page-404__sub-title">タグからも記事を探していただけます。</span>
+          <?php
+          $tags = get_tags(array("orderby" => "count", "order" => "DESC"));
+          ?>
+          <ul class="page-404__tag-list-wrapper">
+            <?php foreach ($tags as $tag): ?>
+              <li class="page-404__tag-list">
+                <a href="<?php echo get_tag_link($tag); ?>" class="page-404__tag-link"><?php echo $tag->name; ?></a>
+              </li>
+            <?php endforeach; ?>
+          </ul>
         </div>
       </div>
       <?php get_sidebar(); ?>
--- wp-content/themes/myTheme/search.php
+++ wp-content/themes/myTheme/search.php
@@ -40,7 +40,43 @@ $count = $wp_query->found_posts;
             ?>
           </div>
         <?php else: ?>
-          <p class="no-loop-text">該当する記事はありませんでした。</p>
+          <div class="no-results">
+            <span class="no-results__message">該当する記事はありませんでした。<br>別のキーワードで検索してみてください。</span>
+            <span class="no-results__sub-title">こちらの記事はいかがですか？</span>
+            <?php
+            $posts = get_posts(array("numberposts" => 4, "orderby" => "rand",));
+            ?>
+            <ul class="no-results__recommend-list-wrapper">
+              <?php foreach ($posts as $post): ?>
+                <li class="no-results__recommend-list">
+                  <a href="<?php echo get_permalink($post->ID) ?>" class="no-results__recommend-link">
+                    <div class="no-results__recommend-image-wrapper">
+                      <?php
+                      $postThumbnail = get_the_post_thumbnail($post->ID, 'medium');
+                      if ($postThumbnail) {
+                        echo $postThumbnail;
+                      } else {
+                        echo "<img src='" . get_template_directory_uri() . "/images/no-thumbnail.jpg' alt='no-thumbnail'>";
+                      }
+                      ?>
+                    </div>
+                    <span class="no-results__recommend-title"><?php echo get_the_title($post->ID) ?></span>
+                  </a>
+                </li>
+              <?php endforeach; ?>
+            </ul>
+            <span class="no-results__sub-title">タグからも記事を探していただけます。</span>
+            <?php
+            $tags = get_tags(array("orderby" => "count", "order" => "DESC"));
+            ?>
+            <ul class="no-results__tag-list-wrapper">
+              <?php foreach ($tags as $tag): ?>
+                <li class="no-results__tag-list">
+                  <a href="<?php echo get_tag_link($tag); ?>" class="no-results__tag-link"><?php echo $tag->name; ?></a>
+                </li>
+              <?php endforeach; ?>
+            </ul>
+          </div>
         <?php endif; ?>
       </div>
       <?php get_sidebar(); ?>
