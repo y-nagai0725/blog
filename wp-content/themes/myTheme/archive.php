e8106c32f5967e43d5ec27e83fe6bdc2fa2ab4f8
y-nagai0725 <y.nagai0725@gmail.com>
2025-01-21 20:02:16 +0900

    アーカイブページにて件数が0の場合の表示を追加

--- wp-content/themes/myTheme/archive.php
+++ wp-content/themes/myTheme/archive.php
@@ -31,28 +31,68 @@ if (is_category()) {
     <?php echo breadcrumb(); ?>
     <div class="container">
       <div class="contents">
-        <ul class="loop-contents">
-          <?php
-          if (have_posts()) {
-            while (have_posts()) {
-              the_post();
-              get_template_part('loop-content');
+        <?php if ($count): ?>
+          <ul class="loop-contents">
+            <?php
+            if (have_posts()) {
+              while (have_posts()) {
+                the_post();
+                get_template_part('loop-content');
+              }
             }
-          }
-          ?>
-        </ul>
-        <div class="pagination">
-          <?php
-          echo paginate_links(
-            array(
-              'type' => 'list',
-              'mid_size' => '1',
-              'prev_text' => '',
-              'next_text' => ''
-            )
-          );
-          ?>
-        </div>
+            ?>
+          </ul>
+          <div class="pagination">
+            <?php
+            echo paginate_links(
+              array(
+                'type' => 'list',
+                'mid_size' => '1',
+                'prev_text' => '',
+                'next_text' => ''
+              )
+            );
+            ?>
+          </div>
+        <?php else: ?>
+          <div class="no-results">
+            <span class="no-results__message">該当する記事はありませんでした。<br>別のキーワードで検索してみてください。</span>
+            <span class="no-results__sub-title">こちらの記事はいかがですか？</span>
+            <?php
+            $posts = get_posts(array("numberposts" => 4, "orderby" => "rand",));
+            ?>
+            <ul class="no-results__recommend-list-wrapper">
+              <?php foreach ($posts as $post): ?>
+                <li class="no-results__recommend-list">
+                  <a href="<?php echo get_permalink($post->ID) ?>" class="no-results__recommend-link">
+                    <div class="no-results__recommend-image-wrapper">
+                      <?php
+                      $postThumbnail = get_the_post_thumbnail($post->ID, 'medium');
+                      if ($postThumbnail) {
+                        echo $postThumbnail;
+                      } else {
+                        echo "<img src='" . get_template_directory_uri() . "/images/no-thumbnail.jpg' alt='no-thumbnail'>";
+                      }
+                      ?>
+                    </div>
+                    <span class="no-results__recommend-title"><?php echo get_the_title($post->ID) ?></span>
+                  </a>
+                </li>
+              <?php endforeach; ?>
+            </ul>
+            <span class="no-results__sub-title">タグからも記事を探していただけます。</span>
+            <?php
+            $tags = get_tags(array("orderby" => "count", "order" => "DESC"));
+            ?>
+            <ul class="no-results__tag-list-wrapper">
+              <?php foreach ($tags as $tag): ?>
+                <li class="no-results__tag-list">
+                  <a href="<?php echo get_tag_link($tag); ?>" class="no-results__tag-link"><?php echo $tag->name; ?></a>
+                </li>
+              <?php endforeach; ?>
+            </ul>
+          </div>
+        <?php endif; ?>
       </div>
       <?php get_sidebar(); ?>
     </div>
